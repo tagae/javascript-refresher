da0d8e8da6cdcb7179a0244f0163ecb91737f228
Sebastián González <sebastian.gonzalez@tomtom.com>
2017-03-26 01:06:38 +0100

    chore: change a few reveal.js settings

--- config.js
+++ config.js
@@ -8,18 +8,26 @@ require('./node_modules/reveal.js/lib/js/head.min.js');
 require('./node_modules/reveal.js/js/reveal.js');
 
 // styling
-require('./node_modules/reveal.js/css/theme/black.css');
+require('./node_modules/reveal.js/css/theme/white.css');
 require('./node_modules/reveal.js/lib/css/zenburn.css');
+require('./index.css');
 
 // plugins
 require('./node_modules/reveal.js/plugin/zoom-js/zoom.js');
 require('./node_modules/reveal.js/plugin/notes/notes.js');
 require('./node_modules/reveal.js/plugin/highlight/highlight.js');
 
+// printing
+require('./print.js')
+
 hljs.initHighlightingOnLoad();
 
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
   slideNumber: true,
-  history: true
+  transition: 'none'
 });
